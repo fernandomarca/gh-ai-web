39ff6700cd87a485946784320e42557e18552713
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2024-12-04 16:38:51 -0300

    chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,116 @@
 import { Github, Wand2 } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { Textarea } from "./components/ui/textarea";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select";
 import { Label } from "./components/ui/label";
 import { Separator } from "./components/ui/separator";
 import { Slider } from "./components/ui/slider";
 import { VideoInputForm } from "./components/video-input-form";
 import { PromptSelect } from "./components/prompt-select";
-import { useState } from "react";
-import { useCompletion } from 'ai/react';
+import { useEffect, useState } from "react";
+import { useCompletion } from "ai/react";
 import { Header } from "./components/header";
+import { api } from "./lib/axios";
+import { consumeReadableStream } from "./lib/consume";
+
+interface MessageChat {
+  model: string;
+  created_at: string;
+  message?: {
+    role: string;
+    content: string;
+  };
+  response?: string;
+  done: boolean;
+}
 
 export function App() {
   const [temperatura, setTemperatura] = useState(0.5);
-  const [videoId, setVideoId] = useState<{
-    videoId: string;
-    transcription: string;
-  } | null>(null);
+  const [chatMessages, setChatMessages] = useState("");
 
-  // const { input, setInput, handleInputChange, handleSubmit, completion, isLoading } = useCompletion({
-  //   api: 'http://localhost:3333/ai/complete',
-  //   body: {
-  //     videoId: videoId?.videoId,
-  //     temperatura,
-  //   },
-  // });
+  async function processResponse(
+    response: Response,
+    setChatMessages?: React.Dispatch<React.SetStateAction<string>>
+  ) {
+    let fullText = "";
+    let contentToAdd = "";
+    if (response.body) {
+      await consumeReadableStream(response.body, (chunk) => {
+        try {
+          // Ollama's streaming endpoint returns new-line separated JSON
+          // objects. A chunk may have more than one of these objects, so we
+          // need to split the chunk by new-lines and handle each one
+          // separately.
+          contentToAdd = chunk
+            .trimEnd()
+            .split("\n")
+            .reduce((acc, line) => {
+              let message = JSON.parse(line) as MessageChat;
+              if (message.response && !message.done) {
+                acc += message.response;
+                return acc;
+              }
+              if (message.message && !message.done) {
+                acc += message.message.content;
+                return acc;
+              }
+              return acc;
+            }, "");
+          fullText += contentToAdd;
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+        }
+        if (setChatMessages) {
+          setChatMessages((prev) => {
+            return prev + contentToAdd;
+          });
+        }
+      });
+      return fullText;
+    } else {
+      throw new Error("Response body is null");
+    }
+  }
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const body2 = {
+      model: "llama3.2",
+      question: "explique o pop 047",
+      collection: "pops",
+      stream: true,
+      temperature: 0.0,
+      num_thread: 4,
+    };
+    const body = {
+      model: "llama3.2",
+      // prompt: "qual a sua ultima atualziacao",
+      messages: [
+        {
+          role: "user",
+          content: "porque o ceu é azul?",
+        },
+      ],
+      stream: true,
+      // format: "json",
+    };
+    const response = await fetch("http://localhost:8081/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    let _result = await processResponse(response, setChatMessages);
+    // let _result = await response.text();
+    setChatMessages(_result);
+  }
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -35,47 +121,42 @@ export function App() {
           <div className="grid grid-rows-2 gap-4 flex-1">
             <Textarea
               className="resize-none p-4 leading-relaxed"
-              placeholder="Resultado da transcrição do vídeo selecionado."
-              value={videoId?.transcription}
+              placeholder="Resultado gerado pela IA."
+              readOnly
+              value={chatMessages}
             />
             {/* <Textarea
               className="resize-none p-4 leading-relaxed"
               placeholder="Inclua o prompt para a IA.."
               value={input}
               onChange={handleInputChange}
-            />
-            <Textarea
-              className="resize-none p-4 leading-relaxed"
-              placeholder="Resultado gerado pela IA."
-              readOnly
-              value={completion}
             /> */}
           </div>
           {/* <p className="text-sm text-muted-foreground">Lembre-se: você pode utilizar a variável <code className="text-violet-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.</p> */}
         </div>
-        <aside className="w-80 space-y-6" >
-          <VideoInputForm onVideoUploaded={setVideoId} />
+        <aside className="w-80 space-y-6">
+          {/* <VideoInputForm onVideoUploaded={setVideoId} /> */}
           <Separator />
 
-          {/* <form onSubmit={handleSubmit} className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
             <div className="space-y-2">
               <Label>Prompt</Label>
-              <PromptSelect onPromptSelected={setInput} />
+              <PromptSelect onPromptSelected={() => {}} />
             </div>
 
             <div className="space-y-2">
               <Label>Modelo</Label>
-              <Select disabled defaultValue="gpt3.5">
+              <Select disabled defaultValue="llama3.2">
                 <SelectTrigger>
                   <SelectValue />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value="gpt3.5">
-                    GPT-3.5-turbo 16k
-                  </SelectItem>
+                  <SelectItem value="llama3.2">llama3.2</SelectItem>
                 </SelectContent>
               </Select>
-              <span className="block text-xs text-muted-foreground italic">Você poderá customizar essa opção em breve</span>
+              <span className="block text-xs text-muted-foreground italic">
+                Você poderá customizar essa opção em breve
+              </span>
             </div>
 
             <Separator />
@@ -87,22 +168,23 @@ export function App() {
                 min={0}
                 step={0.1}
                 value={[temperatura]}
-                onValueChange={value => setTemperatura(value[0])}
+                onValueChange={(value) => setTemperatura(value[0])}
               />
               <span className="block text-xs text-muted-foreground italic leading-relaxed">
-                Valores mais altos geram resultados mais criativos, enquanto valores mais baixos geram resultados mais precisos.
+                Valores mais altos geram resultados mais criativos, enquanto
+                valores mais baixos geram resultados mais precisos.
               </span>
             </div>
 
             <Separator />
 
-            <Button disabled={isLoading} type="submit" className="w-full">
+            <Button disabled={false} type="submit" className="w-full">
               Executar
               <Wand2 className="w-4 h-4 ml-2" />
             </Button>
-          </form> */}
+          </form>
         </aside>
       </main>
-    </div >
-  )
+    </div>
+  );
 }
--- src/components/prompt-select.tsx
+++ src/components/prompt-select.tsx
@@ -1,5 +1,11 @@
 import { useEffect, useState } from "react";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
 import { api } from "@/lib/axios";
 
 interface Prompt {
@@ -12,33 +18,33 @@ interface PromptSelectProps {
   onPromptSelected: (template: string) => void;
 }
 
-
 export function PromptSelect(props: Readonly<PromptSelectProps>) {
-  const [prompts, setPrompts] = useState<Prompt[] | null>(null);
+  const [prompts, setPrompts] = useState<Prompt[] | null>([
+    { id: "1", title: "pop 047", template: "resuma o pop 047" },
+    { id: "2", title: "pop 003", template: "resuma o pop 003" },
+  ]);
 
-  useEffect(() => {
-    api.get('/prompts').then(response => {
-      setPrompts(response.data);
-    });
-  }, []);
+  // useEffect(() => {
+  //   api.get("/prompts").then((response) => {
+  //     setPrompts(response.data);
+  //   });
+  // }, []);
 
   function handlePromptSelected(promptId: string) {
-    const selectedPrompt = prompts?.find(prompt => prompt.id === promptId);
+    const selectedPrompt = prompts?.find((prompt) => prompt.id === promptId);
 
     if (!selectedPrompt) return;
 
     props.onPromptSelected(selectedPrompt.template);
   }
 
-
   return (
     <Select onValueChange={handlePromptSelected}>
       <SelectTrigger>
-        <SelectValue placeholder=
-          "Selecione um prompt..." />
+        <SelectValue placeholder="Selecione um prompt..." />
       </SelectTrigger>
       <SelectContent>
-        {prompts?.map(prompt => {
+        {prompts?.map((prompt) => {
           return (
             <SelectItem key={prompt.id} value={prompt.id}>
               {prompt.title}
@@ -47,5 +53,5 @@ export function PromptSelect(props: Readonly<PromptSelectProps>) {
         })}
       </SelectContent>
     </Select>
-  )
+  );
 }
